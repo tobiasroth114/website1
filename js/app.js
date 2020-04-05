1f382d6c0e1a759f2a9251e91ee89b5fed30b5f9
Tobias Roth <tobiasroth@Tobiass-MBP.fritz.box>
2020-04-05 22:51:59 +0200

    s v

--- js/app.js
+++ js/app.js
@@ -46,34 +46,3 @@ $(".gallery").magnificPopup({
     }
 });
 
-//form-submission
-
-let cta = document.getElementById('cta');
-let email = document.getElementById('email').value;
-
-cta.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (this.email.value == null || this.email.value == "") {
-        error.style.display = 'block';
-    } else {
-        let fetchData = {
-            method: 'POST',
-            body: JSON.stringify({
-                email: this.email.value,
-                js: true
-            }),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        }
-        fetch('/subscribe', fetchData)
-            .then(res => {
-                if (res.ok) {
-                    //yay
-                } else {
-                    error.style.dislay = 'block';
-                }
-            })
-    }
-})  
